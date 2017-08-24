6a331b4850348ded9fca4237ab9efd2176f2e839
v3gaj <juanvegaquesada@gmail.com>
2017-08-23 22:04:16 -0600

    Content

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
 
-	has_attached_file :image, styles: { thumb: "370x250#", large: "1200x500#" }
+	has_attached_file :image, styles: { thumb: "370x250#", large: "1200x800#" }
 	validates_attachment_content_type :image, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/
 
 	default_scope { order(index: :asc) }
