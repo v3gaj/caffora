6542696e9224de3a64b52433b9e4ca766e3bbd5b
v3gaj <juanvegaquesada@gmail.com>
2017-08-27 23:30:39 -0600

    Fixes 4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,12 @@ class PostsController < ApplicationController
     @post.contents = @post.contents.all.order(:index)
     @collection = Collection.find_by_post_id(@post.id)
 
-    
+    @post.contents.each do |content|
+      if content.style === "Paragraph"
+          @page_description = content.body
+        break
+      end
+    end
   end
 
   # GET /posts/new
